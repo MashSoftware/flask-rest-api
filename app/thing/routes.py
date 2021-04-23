c218895fa765b0221fefa9a3a878ae45b5edd133
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-04-23 08:46:54 +0100

    add openapi spec and schema validation

--- app/thing/routes.py
+++ app/thing/routes.py
@@ -4,15 +4,31 @@ from datetime import datetime
 from app import db
 from app.models import Thing
 from app.thing import bp
-from flask import Response, url_for
+from flask import Response, request, url_for
 from flask_negotiate import consumes, produces
+from jsonschema import FormatChecker, ValidationError, validate
+from werkzeug.exceptions import BadRequest
+
+# JSON schema for thing requests
+with open("openapi.json") as json_file:
+    openapi = json.load(json_file)
+thing_schema = openapi["components"]["schemas"]["ThingRequest"]
 
 
 @bp.route("", methods=["GET"])
 @produces("application/json")
 def get_things():
     """Get a list of Things."""
-    things = Thing.query.all()
+    name_query = request.args.get("name", type=str)
+
+    if name_query:
+        things = (
+            Thing.query.filter(Thing.name.ilike("%{}%".format(name_query)))
+            .order_by(Thing.created_at.desc())
+            .all()
+        )
+    else
+        things = Thing.query.order_by(Thing.created_at.desc()).all()
 
     if things:
         results = []
@@ -33,7 +49,14 @@ def get_things():
 @produces("application/json")
 def create_thing():
     """Create a new Thing."""
-    thing = Thing()
+    
+    # Validate request against schema
+    try:
+        validate(request.json, thing_schema, format_checker=FormatChecker())
+    except ValidationError as e:
+        raise BadRequest(e.message)
+
+    thing = Thing(name=request.json["name"])
 
     db.session.add(thing)
     db.session.commit()
@@ -47,7 +70,7 @@ def create_thing():
 @bp.route("/<uuid:thing_id>", methods=["GET"])
 @produces("application/json")
 def get_thing(thing_id):
-    """Get a Thing for a given ID."""
+    """Get a Thing with a specific ID."""
     thing = Thing.query.get_or_404(str(thing_id))
 
     return Response(repr(thing), mimetype="application/json", status=200)
@@ -57,9 +80,17 @@ def get_thing(thing_id):
 @consumes("application/json")
 @produces("application/json")
 def update_thing(thing_id):
-    """Update a Thing for a given ID."""
+    """Update a Thing with a specific ID."""
+    
+    # Validate request against schema
+    try:
+        validate(request.json, thing_schema, format_checker=FormatChecker())
+    except ValidationError as e:
+        raise BadRequest(e.message)
+    
     thing = Thing.query.get_or_404(str(thing_id))
 
+    thing.name = request.json["name"].title()
     thing.updated_at = datetime.utcnow()
 
     db.session.add(thing)
@@ -71,7 +102,7 @@ def update_thing(thing_id):
 @bp.route("/<uuid:thing_id>", methods=["DELETE"])
 @produces("application/json")
 def delete_thing(thing_id):
-    """Delete a Thing for a given ID."""
+    """Delete a Thing with a specific ID."""
     thing = Thing.query.get_or_404(str(thing_id))
 
     db.session.delete(thing)
