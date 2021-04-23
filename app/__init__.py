3ede63fa7a62b49774597ca591e121af3467611b
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-04-23 08:46:35 +0100

    rename errors blueprint to main

--- app/__init__.py
+++ app/__init__.py
@@ -28,9 +28,9 @@ def create_app(config_class=Config):
 
     app.register_blueprint(thing_bp, url_prefix="/v1/things")
 
-    from app.errors import bp as errors_bp
+    from app.main import bp as main_bp
 
-    app.register_blueprint(errors_bp)
+    app.register_blueprint(main_bp)
 
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,20 @@
 import json
 
-from app.errors import bp
+from app.main import bp
 from flask import Response
 from werkzeug.exceptions import HTTPException, InternalServerError
+
+
+@bp.route("/openapi", methods=["GET"])
+def openapi():
+    with open("openapi.json") as openapi_json:
+        openapi = json.load(openapi_json)
+
+    return Response(
+        json.dumps(openapi, separators=(",", ":")),
+        mimetype="application/json",
+        status=200,
+    )
 
 
 @bp.app_errorhandler(HTTPException)
