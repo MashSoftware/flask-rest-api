282af53535d12c7d839534294ca1af5eecf56274
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2021-08-27 21:43:55 +0100

    add app secret key

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import os
 
 
 class Config(object):
+    RATELIMIT_HEADERS_ENABLED = True
+    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "ELHa$YW3rK@Weu9qJaa4b*ryz3Uek%zM"
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
         if os.environ.get("DATABASE_URL")
@@ -9,5 +12,3 @@ class Config(object):
     )
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_size": 20}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
-    RATELIMIT_HEADERS_ENABLED = True
