7e6bf490bf6bea65fad10741f3e9bd9a245cfa77
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-05-31 17:48:13 +0100

    update postgresql port

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
         if os.environ.get("DATABASE_URL")
-        else "postgresql://mash:mash@localhost:5432/thing"
+        else "postgresql://mash:mash@localhost:5433/thing"
     )
     SQLALCHEMY_ENGINE_OPTIONS = {"pool_size": 20}
     SQLALCHEMY_TRACK_MODIFICATIONS = False
