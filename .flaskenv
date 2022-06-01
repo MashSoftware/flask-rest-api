7ceece0f636f63bfbd92376682b8f62cc3fae7ef
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2022-06-01 14:02:57 +0100

    move all config defaults to .env file

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,5 @@
 FLASK_APP=flask_rest_api.py
 FLASK_ENV=development
 FLASK_RUN_PORT=3001
+REDIS_URL=memory://
+SECRET_KEY=2b4a53a82b37a27c8c47f8f74421483ced4e22bfd1897e178e6b8ced49bec3ea
--- config.py
+++ config.py
@@ -3,8 +3,8 @@ import os
 
 class Config(object):
     RATELIMIT_HEADERS_ENABLED = True
-    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL") or "memory://"
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "ELHa$YW3rK@Weu9qJaa4b*ryz3Uek%zM"
+    RATELIMIT_STORAGE_URL = os.environ.get("REDIS_URL")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL").replace("postgres://", "postgresql://")
         if os.environ.get("DATABASE_URL")
